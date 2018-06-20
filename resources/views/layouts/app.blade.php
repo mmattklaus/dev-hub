448a4d6651b20ed9e84080abd056ea693e4144e2
Matt-Klaus <mmattklaus@gmail.com>
2018-06-20 02:33:52 +0100

    Initial commit. Laravel with semantic ui integration.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Dev Hub') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('assets/css/semantic.paper.css') }}" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script type="text/javascript" src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/app.js') }}" async=""></script>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+    <!-- Font Awesome 5 -->
+    <link href="{{ asset('assets/css/font-awesome.css') }}" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div class="ui large top fixed borderless menu">
+        <div class="ui container">
+            <a class="item">
+                <img class="logo" src="{{ asset('assets/images/logo.jpeg') }}"> &nbsp;
+                Hub
+            </a>
+            <a class="item">
+                Messages
+            </a>
+            <a class="item">
+                Friends
+            </a>
+            <div class="right menu">
+                <div class="item">
+                    <div class="ui icon input">
+                        <input type="text" placeholder="Search...">
+                        <i class="search link icon"></i>
+                    </div>
+                </div>
+                <div class="ui item">
+                    <div class="ui primary button">Sign up</div>
+                </div>
+                <div class="item">
+                    <div class="ui button">Log-in</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="ui main container" style="padding-top: 64px!important">
+        <div class="ui grid">
+            <div class="twelve wide column">
+                @yield('content')
+            </div>
+            <div class="four wide column">
+                <div class="ui list">
+                  <div class="item">
+                    <i class="folder icon"></i>
+                    <div class="content">
+                      <div class="header">src</div>
+                      <div class="description">Source files for project</div>
+                      <div class="list">
+                        <div class="item">
+                          <i class="folder icon"></i>
+                          <div class="content">
+                            <div class="header">site</div>
+                            <div class="description">Your site's theme</div>
+                        </div>
+                    </div>
+                    <div class="item">
+                      <i class="folder icon"></i>
+                      <div class="content">
+                        <div class="header">themes</div>
+                        <div class="description">Packaged theme files</div>
+                        <div class="list">
+                          <div class="item">
+                            <i class="folder icon"></i>
+                            <div class="content">
+                              <div class="header">default</div>
+                              <div class="description">Default packaged theme</div>
+                          </div>
+                      </div>
+                      <div class="item">
+                        <i class="folder icon"></i>
+                        <div class="content">
+                          <div class="header">my_theme</div>
+                          <div class="description">Packaged themes are also available in this folder</div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <div class="item">
+          <i class="file icon"></i>
+          <div class="content">
+            <div class="header">theme.config</div>
+            <div class="description">Config file for setting packaged themes</div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+<div class="item">
+    <i class="folder icon"></i>
+    <div class="content">
+      <div class="header">dist</div>
+      <div class="description">Compiled CSS and JS files</div>
+      <div class="list">
+        <div class="item">
+          <i class="folder icon"></i>
+          <div class="content">
+            <div class="header">components</div>
+            <div class="description">Individual component CSS and JS</div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+<div class="item">
+    <i class="file icon"></i>
+    <div class="content">
+      <div class="header">semantic.json</div>
+      <div class="description">Contains build settings for gulp</div>
+  </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+<script type="text/javascript" src="{{ asset('assets/js/semantic.js') }}" defer></script>
+</body>
+</html>
